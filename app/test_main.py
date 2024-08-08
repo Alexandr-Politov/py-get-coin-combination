e5315d6594fad3c31f49f0eb246c7747d86699d0
Alexandr Politov <apolitov83@gmail.com>
2024-08-08 14:45:26 +0300

    Solution Politov_

--- app/test_main.py
+++ app/test_main.py
@@ -2,7 +2,7 @@ from app.main import get_coin_combination
 
 
 def test_normal_function_operation() -> None:
-    assert get_coin_combination(74) == [4, 0, 2, 2]
+    assert get_coin_combination(73) == [3, 0, 2, 2]
 
 
 def test_cents_is_zero_return_list_of_zeros() -> None:
